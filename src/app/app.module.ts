bf797169b52e4d05c69644d8a018ef358fad18f5
sofia lopez <sofi.lopez123@outlook.com.ar>
2022-09-29 21:53:06 -0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentComponent } from './component/content/content.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
